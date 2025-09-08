f8cd4a2849160df06a20c861fcde9a0f1e1a2893
gambl1ng <delacruzvivianaira11@gmail.com>
2025-09-07 19:30:17 -0900

    StudentInfoClass

---
+++ StudentInformationClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03LaboratoryExercise
+{
+    class StudentInformationClass
+    {
+        public static int SetStudentNo = 0;
+        public static int SetContactNo = 0;
+        public static string SetProgram = "";
+        public static string SetGender = "";
+        public static string SetBirthday = "";
+        public static string SetFullName = "";
+    }
+}
